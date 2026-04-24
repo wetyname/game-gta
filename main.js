4fb2a51d055f1346e6948904bc159e0985bc9da9
wetyname <wetyname@gmail.com>
2026-04-24 12:49:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    win.loadFile("index.html");
+    win.setMenu(null);
+}
+
+app.whenReady().then(createWindow);
